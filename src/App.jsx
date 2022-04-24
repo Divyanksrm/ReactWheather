febb1886bdd66da418b07124e90e4c7b2a3c7b13
Divyanksrm <divyanksrm1999@gmail.com>
2022-04-24 19:36:39 +0530

    my first commit reactWebsite

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+
+
+import Temp from "./wheather/wheather";
+
+const App = () => {
+
+  return (
+
+
+    <>
+
+
+
+
+      <Temp />
+
+
+
+
+    </>
+  )
+};
+
+export default App;
